ac572002cc16b2b3ca8722463891f0d0ccd257b6
AFOP <4592@holbertonschool.com>
2022-07-02 20:48:24 -0500

    Update Task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,30 +1,38 @@
 #!/usr/bin/python3
-# that serializes instances to a JSON file and deserializes JSON file to instances
+# that serializes instances to a JSON
+# file and deserializes JSON file to instances
 
 import json
 import os
-from models.base_model import BaseModel
+
 
 class FileStorage:
     # class FileStorage
 
     __file_path = "file.json"
-    __objects = {} 
+    __objects = {}
 
     def all(self):
-    # returns the dictionary __objects
+        """returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
-    # sets in __objects the obj with key <obj class name>.id
+        """sets in __objects the obj with key <obj class name>.id"""
         if obj is not None:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
             self.__objects[key] = obj
 
     def save(self):
-    # serializes __objects to the JSON file (path: __file_path)
-        pass
-    
+        """serializes __objects to the JSON file (path: __file_path)"""
+        with open(self.__file_path, 'w', encoding="utf-8") as f:
+            self.__objects = json.dumps(
+                self.__objects, sort_keys=True, default=str)
+            f.write(self.__objects)
+
     def reload(self):
-    # deserializes the JSON file to __objects
-        pass
+        """deserializes the JSON file to __objects"""
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r', encoding="utf-8") as file_json:
+                self.__objects = json.loads(file_json.read())
+        else:
+            pass
