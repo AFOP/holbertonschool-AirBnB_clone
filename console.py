cded0e19635ac5ba2dc87e97464e95ad33ec846f
AFOP <4592@holbertonschool.com>
2022-07-06 10:25:34 -0500

    Update task 7

--- console.py
+++ console.py
@@ -25,21 +25,21 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, arg):
         """Creates a new instance of BaseModel
         and print its\n"""
-        if not (arg):
-            print("** class name missing **")
-        elif arg not in self.list_className:
-            print("** class doesn't exist **")
-        else:
+        if (arg) and arg in self.list_className:
             my_model = self.list_className[arg]()
             my_model.save()
             print(my_model.id)
+        elif arg not in self.list_className:
+            print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
 
     def do_show(self, arg):
         """Prints the string representation of an instance
         base on the class name and id\n"""
         if arg:
             arg = arg.split()
-            if arg[0] not in self.list_class:
+            if arg[0] not in self.list_className:
                 print("** class doesn't exist **")
             elif len(arg) < 2:
                 print("** instance id missing **")
@@ -58,7 +58,7 @@ class HBNBCommand(cmd.Cmd):
         class name and id\n"""
         if arg:
             arg = arg.split()
-            if arg[0] not in self.list_class:
+            if arg[0] not in self.list_className:
                 print("** class doesn't exist **")
             elif len(arg) < 2:
                 print("** instance id missing **")
@@ -78,19 +78,26 @@ class HBNBCommand(cmd.Cmd):
         instances based or not on the class name\n"""
         if arg:
             arg = arg.split()
-            if arg[0] not in self.list_class:
+            if arg[0] not in self.list_className:
                 print("** class doesn't exist **")
             else:
-                print("[\"{}\"]".format(str(storage.all())))
+                my_list = []
+                for key, obj in storage.all().items():
+                    if obj.__class__.__name__ == arg[0]:
+                        my_list.append(obj.__str__()) 
+                print(my_list)
         else:
-            print("[\"{}\"]".format(str(storage.all())))
+            my_list = []
+            for key, obj in storage.all().items():
+                my_list.append(obj.__str__())
+            print(my_list)
 
     def do_update(self, arg):
         """Updates an instance based on the class name
         and id by adding or updating attribute\n"""
         if arg:
             arg = arg.split()
-            if arg[0] not in self.list_class:
+            if arg[0] not in self.list_className:
                 print("** class doesn't exist **")
             elif len(arg) < 2:
                 print("** instance id missing **")
