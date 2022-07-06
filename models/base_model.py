bb202549a5b9eea870660232c22c713721a27ef3
AFOP <4592@holbertonschool.com>
2022-07-06 13:59:15 -0500

    Update task 4

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 import uuid
 from datetime import datetime
+from xmlrpc.client import _datetime_type
 from models import storage
 
 
@@ -13,13 +14,12 @@ class BaseModel:
         """method constructor with arguments"""
         if kwargs:
             for key, value in kwargs.items():
-                if key != '__class__':
-                    kwargs[key] = value
-                format = '%Y-%m-%dT%H:%M:%S.%f'
-                if key == 'created_at':
-                    value = datetime.strptime(value, format)
-                if key == 'updated_at':
-                    value = datetime.strptime(value, format)
+                if key == "created_at" or key == "updated_at":
+                    format = "%Y-%m-%dT%H:%M:%S.%f"
+                    datetime_obj = datetime.strptime(value, format)
+                    setattr(self, key, datetime_obj)
+                elif key != "__class__":
+                    setattr(self, key, value)               
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
