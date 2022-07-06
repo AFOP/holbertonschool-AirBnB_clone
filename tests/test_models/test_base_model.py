36430b920a8d1bdc1154769c537105b88b29cbbb
AFOP <4592@holbertonschool.com>
2022-07-06 00:01:46 -0500

    Update Unitest

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 """ Test for models/base_model.py"""
 import unittest
+import os
 from models.base_model import BaseModel
 
 
 class  General(unittest.TestCase):
     "Test Class General"
+
+    def test_executable_file(self):
+        "test_executable_file"
+
+        is_read_true = os.access('models/base_model.py', os.R_OK)
+        self.assertTrue(is_read_true)
+
+        is_write_true = os.access('models/base_model.py', os.W_OK)
+        self.assertTrue(is_write_true)
+
+        is_exec_true = os.access('models/base_model.py', os.X_OK)
+        self.assertTrue(is_exec_true)
 
     def test_id(self):
         "method test id"
